1c702f6bf0734b56d91b8ef7edac02c9ed4102fb
ICE6332 <114802623+ICE6332@users.noreply.github.com>
2025-11-15 22:49:31 +0900

    chore: bump version to 0.3.2-alpha.2

--- src/main/java/com/aicompanion/network/AIWebSocketClient.java
+++ src/main/java/com/aicompanion/network/AIWebSocketClient.java
@@ -76,7 +76,7 @@ public class AIWebSocketClient extends WebSocketClient {
     private void sendConnectionInit() {
         String initMessage = "{\"type\":\"connection_init\",\"timestamp\":" +
             (System.currentTimeMillis() / 1000) +
-            ",\"data\":{\"client\":\"minecraft_mod\",\"version\":\"0.3.2-alpha\"}}";
+            ",\"data\":{\"client\":\"minecraft_mod\",\"version\":\"0.3.2-alpha.2\"}}";
         send(initMessage);
     }
 
